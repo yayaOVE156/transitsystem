dc2a70daf63e10484f76f54102eb551dbb04cc33
AceyPe <119972705+AceyPe@users.noreply.github.com>
2023-06-02 18:05:07 +0300

    fixes for the database

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.transit_system {
     requires javafx.fxml;
     requires firebase.admin;
     requires com.google.auth.oauth2;
+    requires com.google.auth;
 
 
     opens com.example.transit_system to javafx.fxml;
